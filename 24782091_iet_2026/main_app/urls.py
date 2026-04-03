1483b334ed57ddba200daef02597a3f8c49e589d
Aga ta <ragilafda@gmail.com>
2026-04-03 21:00:22 +0700

    Lab 3: Big Update [fundamental Database and CRUD]

--- 24782091_iet_2026/main_app/urls.py
+++ 24782091_iet_2026/main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.add_report, name='add_report'),
+    path('update/<int:pk>/', views.update_report, name='update_report'),
+    path('delete/<int:pk>/', views.delete_report, name='delete_report'),
 ]
--- 24782091_iet_2026/main_app/views.py
+++ 24782091_iet_2026/main_app/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Report
+from .forms import ReportForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'main_app/home.html')
+    reports = Report.objects.all()
+    return render(request, 'main_app/home.html', {'reports': reports})
+
+def report_list(request):
+    # Mengambil semua data dari model Report
+    reports = Report.objects.all() 
+    return render(request, 'main_app/report_list.html', {'reports': reports})
+
+def add_report(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ReportForm()
+    return render(request, 'main_app/add_report.html', {'form': form})
+
+from django.shortcuts import get_object_or_404
+
+def update_report(request, pk):
+    report = get_object_or_404(Report, pk=pk)
+    if request.method == "POST":
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('report_list')
+    else:
+        form = ReportForm(instance=report)
+    return render(request, 'main_app/add_report.html', {'form': form})
+
+def delete_report(request, pk):
+    report = get_object_or_404(Report, pk=pk)
+    report.delete()
+    return redirect('report_list')
