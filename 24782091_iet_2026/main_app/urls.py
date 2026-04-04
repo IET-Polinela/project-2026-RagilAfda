a07b936c201df8cbd164db35435c138d27c524d3
Aga ta <ragilafda@gmail.com>
2026-04-05 01:03:41 +0700

    Lab3: Fix Report Section

--- 24782091_iet_2026/main_app/urls.py
+++ 24782091_iet_2026/main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('reports/', views.report_list, name='report_list'),
     path('add/', views.add_report, name='add_report'),
     path('update/<int:pk>/', views.update_report, name='update_report'),
     path('delete/<int:pk>/', views.delete_report, name='delete_report'),
--- 24782091_iet_2026/main_app/views.py
+++ 24782091_iet_2026/main_app/views.py
@@ -1,31 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Report
 from .forms import ReportForm
 
-# Create your views here.
+# READ (Home / daftar laporan)
 def home(request):
-    reports = Report.objects.all()
-    return render(request, 'main_app/home.html', {'reports': reports})
+    return render(request, 'main_app/home.html')
 
 def report_list(request):
-    # Mengambil semua data dari model Report
-    reports = Report.objects.all() 
-    return render(request, 'main_app/report_list.html', {'reports': reports})
+    reports = Report.objects.all()
+    return render(request, 'main_app/report_list.html', {'reports':reports})
 
+# CREATE
 def add_report(request):
     if request.method == "POST":
         form = ReportForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('report_list')
     else:
         form = ReportForm()
+
     return render(request, 'main_app/add_report.html', {'form': form})
 
-from django.shortcuts import get_object_or_404
-
+# UPDATE
 def update_report(request, pk):
     report = get_object_or_404(Report, pk=pk)
+
     if request.method == "POST":
         form = ReportForm(request.POST, instance=report)
         if form.is_valid():
@@ -33,9 +33,11 @@ def update_report(request, pk):
             return redirect('report_list')
     else:
         form = ReportForm(instance=report)
+
     return render(request, 'main_app/add_report.html', {'form': form})
 
+# DELETE
 def delete_report(request, pk):
     report = get_object_or_404(Report, pk=pk)
     report.delete()
-    return redirect('report_list')
+    return redirect('report_list') 
