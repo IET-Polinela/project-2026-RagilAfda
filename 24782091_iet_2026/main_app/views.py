37e96d5c8d306912fedf0314c1cacc3bc3b11cc9
Aga ta <ragilafda@gmail.com>
2026-04-23 22:23:08 +0700

    Lab6: Management Access

--- 24782091_iet_2026/main_app/views.py
+++ 24782091_iet_2026/main_app/views.py
@@ -1,30 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.views import View
-from django.urls import reverse_lazy
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
 from .models import Report
 
 
-# HOME
 def home(request):
     return render(request, 'main_app/home.html')
 
 
-# LIST (boleh semua)
 class ReportListView(ListView):
     model = Report
     template_name = 'main_app/report_list.html'
     context_object_name = 'reports'
 
 
-# DETAIL (boleh semua)
 class ReportDetailView(DetailView):
     model = Report
     template_name = 'main_app/report_detail.html'
 
 
-# CREATE (HANYA ADMIN)
 class ReportCreateView(CreateView):
     model = Report
     fields = ['title', 'category', 'description', 'location']
@@ -33,16 +30,15 @@ class ReportCreateView(CreateView):
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_admin:
-            messages.error(request, "Akses ditolak ❌ (Hanya admin)")
+            messages.error(request, "Akses ditolak. Hanya admin.")
             return redirect('report_list')
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        messages.success(self.request, "Laporan berhasil ditambahkan ✅")
+        messages.success(self.request, "Laporan berhasil ditambahkan.")
         return super().form_valid(form)
 
 
-# UPDATE (HANYA ADMIN)
 class ReportUpdateView(UpdateView):
     model = Report
     fields = ['title', 'category', 'description', 'location']
@@ -51,16 +47,15 @@ class ReportUpdateView(UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_admin:
-            messages.error(request, "Akses ditolak ❌ (Hanya admin)")
+            messages.error(request, "Akses ditolak. Hanya admin.")
             return redirect('report_list')
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        messages.success(self.request, "Laporan berhasil diupdate ✏️")
+        messages.success(self.request, "Laporan berhasil diupdate.")
         return super().form_valid(form)
 
 
-# DELETE (HANYA ADMIN)
 class ReportDeleteView(DeleteView):
     model = Report
     template_name = 'main_app/confirm_delete.html'
@@ -68,21 +63,19 @@ class ReportDeleteView(DeleteView):
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_admin:
-            messages.error(request, "Akses ditolak ❌ (Hanya admin)")
+            messages.error(request, "Akses ditolak. Hanya admin.")
             return redirect('report_list')
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        messages.success(self.request, "Laporan berhasil dihapus 🗑️")
+        messages.success(self.request, "Laporan berhasil dihapus.")
         return super().post(request, *args, **kwargs)
 
 
-# WORKFLOW STATUS (HANYA ADMIN)
 class ReportUpdateStatusView(View):
     def post(self, request, pk):
-
         if not request.user.is_authenticated or not request.user.is_admin:
-            messages.error(request, "Akses ditolak ❌ (Hanya admin)")
+            messages.error(request, "Akses ditolak. Hanya admin.")
             return redirect('report_list')
 
         report = get_object_or_404(Report, pk=pk)
@@ -91,6 +84,5 @@ class ReportUpdateStatusView(View):
         report.status = new_status
         report.save()
 
-        messages.success(request, f"Status berhasil diubah menjadi {new_status} 🚀")
-
+        messages.success(request, f"Status berhasil diubah menjadi {new_status}.")
         return redirect('report_list')
--- 24782091_iet_2026/usermanagement_24782091/admin.py
+++ 24782091_iet_2026/usermanagement_24782091/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_admin', 'is_member')
+
+admin.site.register(CustomUser, CustomUserAdmin)
