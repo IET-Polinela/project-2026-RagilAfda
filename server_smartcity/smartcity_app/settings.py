bcace8ba47c8edc59c2fcef426ae4c753be56e31
Aga ta <ragilafda@gmail.com>
2026-06-17 00:45:33 +0700

    LabSession 14: Setup Configuration 2

--- server_smartcity/smartcity_app/settings.py
+++ server_smartcity/smartcity_app/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'drf_spectacular',
-    'django_scaler',
+    'django_scalar',
     'main_app',
     'about',
     'contacts',
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'drf_spectacular.openapi.AutoSchema',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
